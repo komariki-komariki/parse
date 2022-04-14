834430a03c56360c3ffe4cc41aaabc33b0e9f9a2
komariki-komariki <komaroff.ilya.S@gmail.com>
2022-04-14 16:38:55 +0300

    search

--- 4.py
+++ 4.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import re
 from datetime import datetime
 articles_list = []
-
+KEYWORDS = ['Программирование', 'дизайн', 'фото', 'web', 'python']
 def text_article():
     HEADERS = {
         'Cookie': '_ym_uid=1639148487334283574; _ym_d=1639149414; _ga=GA1.2.528119004.1639149415; _gid=GA1.2.512914915.1639149415; habr_web_home=ARTICLES_LIST_ALL; hl=ru; fl=ru; _ym_isad=2; __gads=ID=87f529752d2e0de1-221b467103cd00b7:T=1639149409:S=ALNI_MYKvHcaV4SWfZmCb3_wXDx2olu6kw',
@@ -47,7 +47,13 @@ def text_article():
         links = baseurl + i.find('a', class_='tm-article-snippet__title-link')['href']
         articles_list.append([[name], [dates], tag_list, [texts], [links]])
 
-    pprint(articles_list)
+def search():
+    if KEYWORDS[0] in articles_list[0][2]:
+        print(articles_list[0][-1])
+    else:
+        print('не найдено')
+
 
 if __name__ == "__main__":
    text_article()
+   search()
